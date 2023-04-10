16f8757601f621dc9a53c043f67f76380fe119e5
Elliot Glaze <elliot.glaze@ITs-MacBook-Pro.local>
2023-04-10 14:49:54 +0100

    Add data types

---
+++ types/index.ts
@@ -0,0 +1,35 @@
+export interface Profile {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    address: Address;
+}
+
+export interface Address {
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    city: string;
+    postcode: string;
+}
+
+export interface Listing {
+    title: string;
+    description: string;
+    askingForMin: number;
+    askingForMax: number;
+    comments: Comment[];
+    offers: Offer[];
+    media: string[];
+    profile: Profile;
+}
+
+export interface Comment {
+    message: string;
+    profile: Profile;
+    postedAt: Date;
+}
+
+export interface Offer extends Comment {
+    offer: number;
+}
