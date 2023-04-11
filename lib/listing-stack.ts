89606ce86abf081821435363b80c1ecc40775cab
Elliot Glaze <elliot.glaze@ITs-MacBook-Pro.local>
2023-04-11 11:10:51 +0100

    Update dynamoDB config - partitionKey, SortKey

--- lib/listing-stack.ts
+++ lib/listing-stack.ts
@@ -21,16 +21,14 @@ export class ListingStack extends Stack {
         const dynamoTable = new Table(this, 'listing', {
             partitionKey: {
                 name: 'listingId',
+                type: AttributeType.NUMBER,
+            },
+            sortKey: {
+                name: 'title',
                 type: AttributeType.STRING,
             },
             tableName: 'listing',
-
-            /**
-             *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
-             * the new table, and it will remain in your account until manually deleted. By setting the policy to
-             * DESTROY, cdk destroy will delete the table (even if it has data in it)
-             */
-            removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+            removalPolicy: RemovalPolicy.DESTROY,
         });
 
         const nodeJsFunctionProps: NodejsFunctionProps = {
