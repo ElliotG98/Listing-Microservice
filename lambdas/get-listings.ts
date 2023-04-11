a30e98e9da3f65d3d930c897c1b608d23bf4a152
Elliot Glaze <elliot.glaze@ITs-MacBook-Pro.local>
2023-04-11 11:17:01 +0100

    Add get listings functionality

--- lambdas/get-listings.ts
+++ lambdas/get-listings.ts
@@ -1,9 +1,21 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+const db = new AWS.DynamoDB.DocumentClient();
 
 export const handler = async (
     event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
 
-    return { statusCode: 200, body: JSON.stringify('Hello, world!') };
+    const params = {
+        TableName: process.env.TABLE_NAME || '',
+    };
+
+    try {
+        const response = await db.scan(params).promise();
+        return { statusCode: 200, body: JSON.stringify(response.Items) };
+    } catch (dbError) {
+        return { statusCode: 500, body: JSON.stringify(dbError) };
+    }
 };
